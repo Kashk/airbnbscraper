b6dfe4dd9ddf60f8f9f73f81810225027316907a
Max VDW <maxvdw@gmail.com>
2017-09-21 23:28:24 +0100

    Removal of old comments and addition of new commentary

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import requests
 import json
-from lxml import html
 
 # New line variable for handling prettyprint
 newLine = '\n\r'
 
+# room list, hard coded for now
 rooms = ['https://www.airbnb.co.uk/rooms/14531512?s=51', 'https://www.airbnb.co.uk/rooms/19278160?s=51', 'https://www.airbnb.co.uk/rooms/19292873?s=51']
+
+# required data with key and value for prettyprint
 requiredData = {
     'name':'Property Name',
     'bathroom_label':'# of Bathrooms',
@@ -21,41 +23,46 @@ def getRoomJson(request):
     # grab text from http request
     rText = request.text
 
+    # trim down the html page
     trimmed = rText[rText.find('<script type="application/json" data-hypernova-key="p3show_marketplacebundlejs"'):]
 
-    start = trimmed.find("<!--")
+    # redundant but keeps it tidy for now
+    startTag = trimmed.find("<!--")
+    endTag = trimmed.find("-->")
 
-    end = trimmed.find("-->")
-
-    data = trimmed[start:end]
+    data = trimmed[startTag:endTag]
 
     return json.loads(data[4:len(data)])
 
 def getListingJson(roomJson):
+    # return the data we're interested in from the overall JSON
     return roomJson['bootstrapData']['reduxData']['marketplacePdp']['listingInfo']['listing']
 
 def getAmenityJson(listingJson):
+    # return the amenity data
     return listingJson['listing_amenities']
 
-#print (requests.get('https://www.airbnb.co.uk/rooms/20143243').text)
-#r = requests.get('https://www.airbnb.co.uk/rooms/20143243').text
-
-"""
-h = open('headers.txt','w', encoding="utf-8")
-for header in loaded:
-    h.write( header + newLine )
-"""
-
 def printListingData(filename, listingJson):
+    # loop through each attribute in the listing
     for attribute in listingJson:
+
+        # if it's something we're interested in, add it to the results
         if attribute in requiredData:
+
+            # grabs the human friendly name for the attribute E.G. "Property Name" for "localized_room_type"
             filename.write( getPrettyAttributeName(attribute) + ': ' + str( listingJson[attribute] ) + newLine)
 
 def printAmenityData(filename, amenityJson):
     # Iterator for unnamed elements in json
     i = 0
+
+    # loop through each amenity
     for amenity in amenityJson:
+
+        # all amenities are listed, but only those which 'is_present' are the ones we're interested in
         if amenity['is_present']:
+
+            # amenity names are pretty enough, no need to tidy
             filename.write(amenityJson[i]['name'] + newLine)
         i += 1
 
@@ -64,21 +71,28 @@ def printListingTitle(filename):
 
 def printAmenityTitle(filename):
     filename.write(newLine + '==== AMENITIES ====' + newLine)
-#listingDataAmenities = listingData['listing_amenities']
-
-
-
-#f = open('airbnb.txt','w', encoding="utf-8")
-#f.write( data )
 
 if __name__ == "__main__":
+    # opens / creates results file
     resultsFile = open('results.txt','w', encoding="utf-8")
+
+    # loop through each room we're interested in
     for room in rooms:
+        # make the request, get the overall JSON
         roomData = getRoomJson(requests.get(room))
+
+        # get the listing JSON
         listingData = getListingJson(roomData)
+
+        # get the amenity JSON
         amenityData = getAmenityJson(listingData)
 
         printListingTitle(resultsFile)
+
+        #loop around and print each attribute we need
         printListingData(resultsFile,listingData)
+
         printAmenityTitle(resultsFile)
+
+        # loop around and print all present amenities
         printAmenityData(resultsFile,amenityData)
